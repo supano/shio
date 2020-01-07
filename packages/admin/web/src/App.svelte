23dee555f207eceb3b4a76fae7c1e09cd0ccc62c
Supachai Suthikeeree <supano1995@gmail.com>
2020-01-07 15:33:43 +0700

    [Update] switch svelte builder rollup to webpack

---
+++ packages/admin/web/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+import createRouter from '@spaceavocado/svelte-router';
+import RouterView from '@spaceavocado/svelte-router/component/view';
+
+// View components
+import HomePage from './pages/Home.svelte';
+import LoginPage from './pages/Login.svelte';
+import Page404 from './pages/404.svelte';  
+
+createRouter({
+  routes: [
+    { 
+      path: '/',
+      name: 'HOME',
+      component: HomePage,
+	},
+	    { 
+      path: '/login',
+      name: 'Login',
+      component: LoginPage,
+    },
+    {
+      path: '*',
+      component: Page404,
+    },
+  ],
+    mode: 'HISTORY',
+});
+</script>
+
+<RouterView />
